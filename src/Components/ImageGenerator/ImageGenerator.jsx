d46010ede70392ca40e52b6e4ef2e93855388963
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-04-29 00:54:07 +0530

    import image

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -10,6 +10,10 @@ export const ImageGenerator = () => {
         <div className="img-loading">
             <div className="image"><img src={default_image} alt = "" /></div>
         </div>
+        <div className='search-box'>
+            <input type='text' className='search-input' placeholder='Describe What You Want TO See'/>
+            <div className="generate-btn">Generate</div>
+        </div>
     </div>
   )
 }
