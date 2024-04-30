f1d6b6eecdadc8d6d03463e49305aec755fe2c64
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-04-30 16:29:06 +0530

    conect OpenAI API

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './ImageGenerator.css'
 import default_image from '../Assets/default_image.svg'
 
 export const ImageGenerator = () => {
+
+      const [image_url, setImage_url] = useState("/");
+      let inputRef = useRef(null);
+
+      const ImageGenerator = async () =>{
+        if(inputRef.current.value===""){
+          return 0;
+        }
+        const response = await fetch()
+      }
   return (
     <div className='ai-image-generator'>
         <div className="header">Ai image <span>generator</span>
         </div>
         <div className="img-loading">
-            <div className="image"><img src={default_image} alt = "" /></div>
+            <div className="image"><img src={image_url==="/"?default_image:image_url} alt = "" /></div>
         </div>
         <div className='search-box'>
-            <input type='text' className='search-input' placeholder='Describe What You Want TO See'/>
+            <input type='text' ref={inputRef} className='search-input' placeholder='Describe What You Want TO See'/>
             <div className="generate-btn">Generate</div>
         </div>
     </div>
